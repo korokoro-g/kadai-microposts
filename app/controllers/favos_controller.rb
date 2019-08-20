5706e2cfeab5ff3ecbd6b9518fced56979f7c15e
koro.koro <ruby5lesson@gmail.com>
2019-08-20 06:20:01 +0000

    revise

--- app/controllers/favos_controller.rb
+++ app/controllers/favos_controller.rb
@@ -5,13 +5,13 @@ class FavosController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = '投稿をお気に入りに登録しました。'
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = '投稿のお気に入りを解除しました。'
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 end
